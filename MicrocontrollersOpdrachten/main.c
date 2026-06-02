285088b122fd9716d565da86a446d854cbbc9aaa
davidVeer <davidvdveer05@gmail.com>
2026-06-02 12:15:01 +0200

    Setup Repo and project

---
+++ MicrocontrollersOpdrachten/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 6/2/2026 12:05:09 PM
+ *  Author: david
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
