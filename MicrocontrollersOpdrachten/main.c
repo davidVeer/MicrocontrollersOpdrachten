4ac87d68bf80f893dd21ea7517bcd5c0cab22f32
davidVeer <davidvdveer05@gmail.com>
2026-07-03 12:25:39 +0200

    2.4

--- MicrocontrollersOpdrachten/main.c
+++ MicrocontrollersOpdrachten/main.c
@@ -35,49 +35,36 @@ void wait( int ms ) {
 	}
 }
 
-const unsigned char Characters[16] = {
-	0b00111111, // 0
-	0b00000110, // 1
-	0b01011011, // 2
-	0b01001111, // 3
-	0b01100110, // 4
-	0b01101101, // 5
-	0b01111101, // 6
-	0b00000111, // 7
-	0b01111111, // 8
-	0b01101111, // 9
-	0b01110111, // A
-	0b01111100, // B
-	0b00111001, // C
-	0b01011110, // D
-	0b01111001, // E
-	0b01110001, // F
+typedef struct {
+	unsigned char data;
+	unsigned int delay;
+} PATTERN_STRUCT;
+
+#define SEG_A 0b00000001
+#define SEG_B 0b00000010
+#define SEG_C 0b00000100
+#define SEG_D 0b00001000
+#define SEG_E 0b00010000
+#define SEG_F 0b00100000
+#define SEG_G 0b01000000
+
+PATTERN_STRUCT pattern[] = {
+	// Rondlopend effect (buitenring): a -> b -> c -> d -> e -> f -> a
+	{SEG_A, 100},
+	{SEG_B, 100},
+	{SEG_C, 100},
+	{SEG_D, 100},
+	{SEG_E, 100},
+	{SEG_F, 100},
+	{0x00,  100},
+
+	// Knipperend "8" effect
+	{0b01111111, 200},
+	{0x00,       200},
+	{0b01111111, 200},
+	{0x00,       200},
 };
 
-void setDisplay(int num){
-	if(num > 15 || num < 0){
-		PORTA = Characters[14];
-		} else {
-		PORTA = Characters[num];
-	}
-}
-
-/*****************************************************************
-short:			ISR INT0
-inputs:
-outputs:
-notes:			Set PORTD.5
-Version :    	DMK, Initial code
-*******************************************************************/
-ISR( INT0_vect ) {
-   		bitshift++;
-   		setDisplay(bitshift);
-}
-
-ISR( INT1_vect ) {
-	bitshift--;
-	setDisplay(bitshift);
-}
 
 /******************************************************************
 short:			main() loop, entry point of executable
@@ -91,20 +78,19 @@ int main( void ) {
 	DDRD = 0xF0;			// PORTD(7:4) output, PORTD(3:0) input	
 	DDRA = 0xFF;
 
-	// Init Interrupt hardware
-	EICRA |= 0x07;		// INT1 falling edge, INT0 rising edge
-	EIMSK |= 0x03;			// Enable INT1 & INT0
-	
-	// Enable global interrupt system
-	//SREG = 0x80;			// Of direct via SREG of via wrapper
-	sei();				
-
 	for(;;) {	
-		if(PIND == 0b11111111){
-			bitshift = 0;
-			setDisplay(bitshift);
-		}						
-	}
+		int index = 0;
 
+		// loop through the array
+		for (int i = 0; i < sizeof(pattern)/sizeof(pattern[0]); i++)
+		{
+			// Write data to PORTA
+			PORTA = pattern[index].data;
+			// wait
+			wait(pattern[index].delay);
+			// increment for next round
+			index++;
+		}
+	}
 	return 1;
 }
